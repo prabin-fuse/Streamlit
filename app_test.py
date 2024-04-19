b4d99a88ada016a4bf925b9a016f3a4f0fc1c62b
Prabin Bohara <prabin.bohara@fusemachines.com>
2024-04-19 11:07:46 +0545

    Basic functionality of streamlit done

---
+++ app_test.py
@@ -0,0 +1,49 @@
+import streamlit as st
+
+# Title: Used to add title of an app
+st.title("HEllo Prabin to streamlit")
+
+# Header:
+st.header("Header ")
+
+st.subheader("Sub header part")
+
+# Information:
+st.info("Information details of the user")
+
+st.success("Success! Congratulations")
+
+#warning:
+st.warning("This is warning. Please be careful")
+
+# Error message:
+st.error("Error msg. Wrong password")
+
+# Write function:
+st.write("Write: First user is : ")
+
+#text function:
+st.text("Text: is here")
+
+#coding format:
+st.write(range(50))
+
+# Markdown:
+st.markdown("# Markdown Prabin")
+st.markdown("## Markdown Prabin level2")
+st.markdown("### Markdown Prabin level3")
+st.markdown(":moon:")
+
+# Caption:
+st.caption("Caption : Good")
+
+# Mathematical equations using Latex:
+st.latex(r''' a+b x^2+c''')
+
+#Image:
+st.image("fusemachines.png")
+
+
+
+
+
